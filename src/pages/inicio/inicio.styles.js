cab467352a60f8286c75cd2c9debf84a907d81b1
Viridiana-24 <Viridiana_rdz24@hotmail.com>
2022-04-17 01:12:31 -0500

    agregados esteticos a la app, agregado de la carpeta autores junto con sus respectivos archivos

---
+++ src/pages/inicio/inicio.styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '##ff7f50',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
