a47e8a696dccc640d4098b4b56a39273de44a24a
retoiteitor190 <63795517+retoiteitor190@users.noreply.github.com>
2022-04-17 12:41:19 -0500

    configuracion amplify

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
 
-export default function App() {
+import Amplify from "aws-amplify";
+
+import awsconfig from "./src/aws-exports";
+
+import { withAuthenticator } from "aws-amplify-react-native";
+
+import MainNavigator from "./src/components/Navigator";
+
+import { GlobalProvider } from "./src/context/global/global.context";
+
+Amplify.configure(awsconfig);
+
+ function App({authData}) {
+   console.log({authData});
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <GlobalProvider>
+      <MainNavigator/>
+    </GlobalProvider>
+  ); 
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default withAuthenticator(App);
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,48 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo {
+    onCreateTodo {
+      id
+      nombre
+      descripcion
+      estatus
+      iSBN
+      categoria
+      fechapublicacion
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo {
+    onUpdateTodo {
+      id
+      nombre
+      descripcion
+      estatus
+      iSBN
+      categoria
+      fechapublicacion
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo {
+    onDeleteTodo {
+      id
+      nombre
+      descripcion
+      estatus
+      iSBN
+      categoria
+      fechapublicacion
+      createdAt
+      updatedAt
+    }
+  }
+`;
